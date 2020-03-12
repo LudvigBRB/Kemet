eb9ad3aa01f18c20c21accfdbd7793b6dc9888d5
ITE17 <cph-lb270@cphbusiness.dk>
2020-03-12 18:01:32 +0100

    begyndte på inventory, virker ikke endnu

---
+++ Assets/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    enum ItemTypes { Seed, Crop };
+    int[] items = new int[2]; //array
+
+    void GiveSeed()
+    {
+        ++items[(int)ItemTypes.Seed];
+    }
+
+    void TakeSeed()
+    {
+        --items[(int)ItemTypes.Seed];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        items[(int)ItemTypes.Seed] = 4;
+        items[(int)ItemTypes.Crop] = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        guiText.text = items[(int)ItemTypes.Seed].ToString();
+    }
+}
