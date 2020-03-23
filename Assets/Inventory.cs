fa7542d702dd6cb4833e9776bd7ab0e9d6d5146e
ITE17 <cph-lb270@cphbusiness.dk>
2020-03-23 12:18:11 +0100

    made basal inventory functionallity

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -8,28 +8,55 @@ public class Inventory : MonoBehaviour
     enum ItemTypes { Seed, Crop };
     int[] items = new int[2]; //array
 
-    public Text myText;
+    public Text seedText;
+    public Text seedInfoText;
+    public Text cropText;
+    public Text cropInfoText;
 
-    void GiveSeed()
+    void SeedControl()
     {
-        ++items[(int)ItemTypes.Seed];
+        if (Input.GetKey("y"))
+        {
+            ++items[(int)ItemTypes.Seed];
+        }
+        if (Input.GetKey("h") && items[(int)ItemTypes.Seed] != 0)
+        {
+            --items[(int)ItemTypes.Seed];
+        }
     }
 
-    void TakeSeed()
+    void CropControl()
     {
-        --items[(int)ItemTypes.Seed];
+        if (Input.GetKey("u"))
+        {
+            ++items[(int)ItemTypes.Crop];
+        }
+        if (Input.GetKey("j") && items[(int)ItemTypes.Crop] != 0)
+        {
+            --items[(int)ItemTypes.Crop];
+        }
     }
+
 
     // Start is called before the first frame update
     void Start()
     {
         items[(int)ItemTypes.Seed] = 4;
         items[(int)ItemTypes.Crop] = 0;
+
+        seedInfoText.text = "Number of seeds";
+        cropInfoText.text = "Number of crops";
     }
 
     // Update is called once per frame
     void Update()
     {
-        myText.text = items[(int)ItemTypes.Seed].ToString();
+        SeedControl();
+        seedInfoText.text.ToString();
+        seedText.text = items[(int)ItemTypes.Seed].ToString();
+
+        CropControl();
+        cropInfoText.text.ToString();
+        cropText.text = items[(int)ItemTypes.Crop].ToString();
     }
 }
