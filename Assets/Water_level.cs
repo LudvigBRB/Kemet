6cb24bc90b4dee3ce9e1b7bd68dad93e382f6fca
msKlan <michaelsanderklan@gmail.com>
2020-04-15 14:29:36 +0200

    added working water

--- Assets/Water_level.cs
+++ Assets/Water_level.cs
@@ -1,55 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
-//testing
 public class Water_level : MonoBehaviour
 {
 
-    //[SerializeField] float moveSpeed = 10f;
-
-    //float y = 0f;
     public GameObject Water;
-    private Vector3 waterVector1 = new Vector3(0.0f, 5.0f, 0.0f); //higehst position of the water
-    private Vector3 waterVector2 = new Vector3(0.0f, -5.0f, 0.0f); //lowest position of the water
-    //private float waterSpeed = 20f;
     private float timer = 0f;
     public TextMesh timertext;
+    private float waterspeed = 5f;
+    public Vector3 waterPosition;
+    private bool waterUpDown = false;
+    private float y1 = 0.01f;
+    private float y2 = -0.01f;
 
-    void Move()
+
+    void Start()
     {
-        //    transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
-        //transform.Translate(Vector3.up * moveSpeed);
-        //transform.Translate(0f, 1f * moveSpeed, 0f);
+        timer = Time.deltaTime;
+
     }
 
-    public void Flow() //transforms water height to low
-    {
 
-        transform.Translate(waterVector1);
 
-    }
-    public void Eb() //transforms water height to high
-    {
-
-        transform.Translate(waterVector2);
-    }
 
     private void FixedUpdate()
     {
-        timer += Time.deltaTime; //counts time since last frame
 
-        if (Mathf.Approximately(timer, 3)) //after 3 frames, place water at lowest position
+
+
+
+        if (timer >= 10)
+            waterUpDown = true;
+        if (timer <= 0)
+            waterUpDown = false;
+
+        if (waterUpDown == false)
         {
-            Flow();
+            timer += Time.deltaTime;
+            transform.Translate(new Vector3(0.0f, y1, 0.0f));
         }
-        if (Mathf.Approximately(timer, 6)) //after another 3 frames, place water at highest position
+        else
         {
-            Eb();
-            timer = 0f; //reset timer to 0
+            timer -= Time.deltaTime;
+            transform.Translate(new Vector3(0.0f, y2, 0.0f));
         }
-        timertext.text = timer.ToString(); //write text
+
+        timertext.text = timer.ToString();
+
+
+
+        //    transform.Translate(new Vector3(0.0f, y, 0.0f));
+
+        //   GameObject.Find("Water").transform.position = waterPosition;
+
+
 
     }
 }
