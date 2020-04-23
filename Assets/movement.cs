3092211942d90c3c818ab46b3a7eea60efbff593
ITE17 <cph-lb270@cphbusiness.dk>
2020-04-23 17:27:06 +0200

    fixed rotation of player

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -37,11 +37,14 @@ public class movement : MonoBehaviour
 
     void Move()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        //transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
     }
 
     void Rotate()
     {
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        /*
         if (Input.GetKey("d"))
         {
             y = y + 0.05f;
@@ -58,9 +61,10 @@ public class movement : MonoBehaviour
         if (Input.GetKey("s"))
         {
             x = x - 0.05f;
-        }
+        }*/
 
-        transform.Rotate(0, y, 0);
+        //transform.Rotate(0, y, 0);
+
     }
 
     /*   void OnCollisionEnter(Collision collision)
