ffef75a224834506669b74c1c0227d92a50edab4
ITE17 <cph-lb270@cphbusiness.dk>
2020-06-10 12:18:32 +0200

    beer and bread functions

---
+++ Assets/BreadShopController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadShopController : MonoBehaviour
+{
+
+    //public Inventory textMa;
+    public ThingController controller;
+
+    void SellBread()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit; // : RaycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 10.0f))
+            {
+                if (hit.collider.gameObject.tag == "bread_shop")
+                {
+                    //textMa.CropSell();
+                    controller.BreadSell();
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SellBread();
+    }
+}
---
+++ Assets/BrewingController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrewingController : MonoBehaviour
+{
+
+    public ThingController controller;
+
+    // Start is called before the first frame update
+    void BrewBeer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit; // : RaycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 10.0f))
+            {
+                if (hit.collider.gameObject.tag == "jar")
+                {
+                    controller.MakeBeer();
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BrewBeer();
+    }
+}
